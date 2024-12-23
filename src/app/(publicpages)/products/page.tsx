8b6acd87c899722983cf70944aa4d303fdb3b624
Huzaifa19999 <huzaifabhai10@gmail.com>
2024-12-23 13:34:30 +0500

    ethereal

--- src/app/(publicpages)/products/page.tsx
+++ src/app/(publicpages)/products/page.tsx
@@ -1,56 +1,135 @@
-import Footer from '@/app/components/Footer'
-import Navbar from '@/app/components/Navbar'
-import Image from 'next/image'
-import React from 'react'
+import Footer from "@/app/components/Footer";
+import Navbar from "@/app/components/Navbar";
+import Image from "next/image";
+import React from "react";
 
 function Products() {
   return (
     <>
-    <Navbar/>
-    <section>
-    <Image alt='cover' src={'/products/Frame 137.png'} className='w-full' height={1600} width={1600} />
-    <section>
-        <div>
-            <p>All</p>
-            <p>Filter By</p>
-            <ul>
-            <li>Size</li>
-            <li><input type="checkbox" />  S(2)</li>
-            <li><input type="checkbox" />  M(2)</li>
-            <li><input type="checkbox" />  L(2)</li>
-            <li><input type="checkbox" />  XL(2)</li>
-            </ul>
-            <ul>
-            <li>Product</li>
-            <li><input type="checkbox" />  Lips</li>
-            <li><input type="checkbox" />  Hairs</li>
-            <li><input type="checkbox" />  Nails</li>
-            <li><input type="checkbox" />  Eyes</li>
-            <li><input type="checkbox" />  Face</li>
-            </ul>
-            <ul>
-            <li>Availability</li>
-            <li><input type="checkbox" />  In Stock</li>
-            <li><input type="checkbox" />  Not Available</li>
-            </ul>
-            <ul>
-            <li>Price</li>
-            <li><input type="checkbox" />  In Stock</li>
-            </ul>
-            <div>
-            <p>Tags</p>
-            <hr className='w-3/12 ' />
-            <br />
-            <button className='border-2 border-black rounded-lg p-1'>Makeup</button>
-            <button className='border-2 border-black rounded-lg p-1'>Face Cream</button>
-            <button className='border-2 border-black rounded-lg p-1'>Skin Care</button>
+      <Navbar />
+      <section>
+        <Image
+          alt="cover"
+          src={"/products/Frame 137.png"}
+          className="w-full"
+          height={1600}
+          width={1600}
+        />
+        <section className="flex flex-col lg:flex-row justify-around px-4 md:px-20">
+
+          {/* Filters Section */}
+          <div className="flex flex-col lg:w-3/12 md:w-4/12 w-full mx-auto mt-10 lg:mt-0">
+            <div className="leading-loose">
+              <p className="font-bold mt-10 dynamic-hover text-lg">All</p>
+              <br />
+              <p className="font-bold text-lg dynamic-hover">Filter By</p>
+              <br />
+              {/* Size Filter */}
+              <ul>
+                <li className="font-bold dynamic-hover text-lg">Size</li>
+                <li>
+                  <input type="checkbox" /> S(2)
+                </li>
+                <li>
+                  <input type="checkbox" /> M(2)
+                </li>
+                <li>
+                  <input type="checkbox" /> L(2)
+                </li>
+                <li>
+                  <input type="checkbox" /> XL(2)
+                </li>
+              </ul>
+              <br />
+              {/* Product Filter */}
+              <ul>
+                <li className="font-bold dynamic-hover text-lg">Product</li>
+                <li>
+                  <input type="checkbox" /> Lips
+                </li>
+                <li>
+                  <input type="checkbox" /> Hairs
+                </li>
+                <li>
+                  <input type="checkbox" /> Nails
+                </li>
+                <li>
+                  <input type="checkbox" /> Eyes
+                </li>
+                <li>
+                  <input type="checkbox" /> Face
+                </li>
+              </ul>
+              <br />
+              {/* Availability Filter */}
+              <ul>
+                <li className="font-bold text-lg dynamic-hover">Availability</li>
+                <li>
+                  <input type="checkbox" /> In Stock
+                </li>
+                <li>
+                  <input type="checkbox" /> Not Available
+                </li>
+              </ul>
+              <br />
+              {/* Price Filter */}
+              <ul>
+                <li className="font-bold dynamic-hover text-lg">Price</li>
+                <li>13:00 - 44.00</li>
+                <li>
+                  <input type="checkbox" /> In Stock
+                </li>
+              </ul>
+              <br />
+              {/* Tags Section */}
+              <div>
+                <p className="font-bold dynamic-hover text-lg">Tags</p>
+                <hr className="w-4/12 border-2" />
+                <br />
+                <div className="flex text-slate-400 gap-3 flex-wrap">
+                  <button className="border-2 border-slate-400 rounded-2xl p-1 px-2">
+                    Makeup
+                  </button>
+                  <button className="border-2 border-slate-400 rounded-2xl p-1 px-2">
+                    Face Cream
+                  </button>
+                  <button className="border-2 border-slate-400 rounded-2xl p-1 px-2">
+                    Skin Care
+                  </button>
+                </div>
+                <br />
+              </div>
+              {/* Image Below */}
+              <div>
+                <Image
+                  src={"/products/Rectangle 163.png"}
+                  height={200}
+                  width={300}
+                  alt="below"
+                />
+              </div>
             </div>
-        </div>
-    </section>
-    </section>
-    <Footer/>
+          </div>
+
+          {/* Products Section */}
+          <div className="w-full lg:w-8/12 flex flex-col lg:flex-row justify-start mt-20 lg:mt-0">
+            <div className="w-full lg:w-11/12 flex flex-col  justify-start">
+            <div className="flex mt-10 lg:justify-end md:justify-start">
+              <p className="mt-2 text-slate-300">Sort by:</p>
+              <select className="border-2 h-10 ms-4 p-2 w-60 rounded-2xl">
+                <option value="">Relevance</option>
+              </select>
+            </div>
+            <hr className="border-2 border-black mt-5 w-full" />
+            </div>
+            {/* Add Product Listings Below */}
+            {/* Add Product Card Components Here */}
+          </div>
+        </section>
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Products
+export default Products;
